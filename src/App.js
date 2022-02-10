d6be63fe8df29c549aaa28c2435d0101f4e8949a
Champak's World <champaksworld@gmail.com>
2022-02-10 19:53:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import VsjExpenseCalculator from './components/VsjExpenseCalculator';
+function App() {
+  return (
+    <div className="App">
+      
+      <VsjExpenseCalculator></VsjExpenseCalculator>
+    </div>
+  );
+}
+
+export default App;
