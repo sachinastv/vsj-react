32ee97db69b5220f533e6cf30fb5e506e1535e93
Champak's World <champaksworld@gmail.com>
2022-02-21 23:39:52 +0530

    Passing the state up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VsjA from './components/VsjA';
-import VsjB from './components/VsjB';
 
-const mydata=0;
-const saveData=(n1)=>{
-  const data={...n1};
-  console.log("App");
-  console.log(data);
-  mydata=data;
-};
-function App() {
+
+//
+
+function App(props) {
+  const[mydata,setMyData]=useState(0);
+  const saveData=(n1)=>{
+    
+    const data={...n1};
+    console.log("App");
+    
+    setMyData(n1);
+    console.log(mydata);
+  };
   return (
     <div className="App">
+      <h2>from VsjA {mydata}</h2>
       <VsjA onSaveData={saveData}/>
-     <VsjB/>
+     
      
     </div>
   );
