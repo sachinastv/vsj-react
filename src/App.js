8ce263de7047dca2d2b9c8c7cadd19e5383a42e8
Champak's World <champaksworld@gmail.com>
2022-02-23 22:52:04 +0530

    Beginning API Calling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import VsjStyle from './components/VsjStyle';
+
 
 
 
 //
 
 function App(props) {
-  const[mydata,setMyData]=useState(0);
-  const saveData=(n1)=>{
+  const [movies,setMovies]=useState([{title:""}]);
+ async function getMoviesHandler()
+  {
+ const response=  await fetch("https://swapi.dev/api/films");
+ console.log(response);
+ const data=await response.json();
+ console.log(data);
+     setMovies( data.results);
     
-    const data={...n1};
-    console.log("App");
-    
-    setMyData(n1);
-    console.log(mydata);
-  };
+  }
+  
   return (
     <div className="App">
-      <h2>from VsjStyle {mydata}</h2>
-      <VsjStyle onSaveData={saveData}/>
-     
+      <button onClick={getMoviesHandler}>Click</button>
+      <h2>Star Wars API</h2>
+     <h2>{movies[0].title}</h2>
      
     </div>
   );
