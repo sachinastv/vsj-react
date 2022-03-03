49a5522b09485a20ad3374d7649eca721bd130ae
Champak's World <champaksworld@gmail.com>
2022-03-03 09:04:41 +0530

    Looping through a map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,23 +8,30 @@ import './App.css';
 //
 
 function App(props) {
-  const [movies,setMovies]=useState([{title:""}]);
- async function getMoviesHandler()
-  {
- const response=  await fetch("https://swapi.dev/api/films");
- console.log(response);
- const data=await response.json();
- console.log(data);
-     setMovies( data.results);
-    
+
+
+  const [movies, setMovies] = useState([{ title: "" }]);
+  async function getMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setMovies(data.results);
+
   }
-  
+
   return (
     <div className="App">
       <button onClick={getMoviesHandler}>Click</button>
       <h2>Star Wars API</h2>
-     <h2>{movies[0].title}</h2>
-     
+      <h2>{movies[0].title}</h2>
+      <h2>{movies.length}</h2>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>{movie.title}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
