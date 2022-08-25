e5bc1a8acf5fa6d823265ab183907ed5531caa08
Champak's World <champaksworld@gmail.com>
2022-08-25 11:12:02 +0530

    Calling the Weather API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import VsjForm from './components/VsjForm';
+import Weather from './components/Weather';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
@@ -10,10 +11,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+<Weather></Weather>
      <h1>Welcome to VSJ React</h1>
      <VsjForm></VsjForm>
-	
+	<Weather></Weather>
     </div>
   );
 }
