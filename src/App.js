d865561f3da6722771bf29260a9d5256bedaf5b7
Champak's World <champaksworld@gmail.com>
2022-08-26 09:35:53 +0530

    Simple One Page Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import VsjForm from './components/VsjForm';
 import Weather from './components/Weather';
+import About from './components/About';
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
+
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
@@ -9,14 +18,26 @@ import './App.css';
 //
 
 function App() {
-  return (
-    <div className="App">
-<Weather></Weather>
-     <h1>Welcome to VSJ React</h1>
-     <VsjForm></VsjForm>
-	<Weather></Weather>
-    </div>
-  );
+  
+    return (
+       <Router>
+           <div className="App">
+           <ul>
+    
+      <li>
+        <Link to="/about">About Us</Link>
+      </li>
+    
+    </ul>
+    <Routes>
+                 
+                 <Route exact path='/about' element={< About />}></Route>
+                 
+          </Routes>
+           </div>
+       </Router>
+   );
+  
 }
 
 export default App;
