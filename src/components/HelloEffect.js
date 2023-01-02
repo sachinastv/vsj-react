1f168bb83450d738341ebc3e4a8853e1a28c7d5c
Champak's World <champaksworld@gmail.com>
2023-01-02 17:41:00 +0530

    SWAPI using react

--- src/components/HelloEffect.js
+++ src/components/HelloEffect.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { useState, useEffect } from "react";
-
+import axios from "axios";
 
 function HelloEffect() {
-    const [count, setCount] = useState(0);
+    const baseURL = "https://swapi.py4e.com/api/people/1/";
 
-    useEffect(() => {
-      setTimeout(() => {
-        setCount((count) => count + 1);
-      }, 1000);
-    });
-  
-    return <h1>{count}</h1>;
-    }
+
+    const [star, setStar] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setStar(response.data);
+        });
+    }, []);
+
+    if (!star) return <h1>No Data</h1>;
+    return <div>
+        <p>{star.name}</p>
+        <p>{star.mass}</p></div>;
+}
 
 
 export default HelloEffect;
