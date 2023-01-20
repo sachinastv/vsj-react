4a3bd6ece583e3ca5a2f22121a56fd3b6d7395f1
Champak's World <champaksworld@gmail.com>
2023-01-20 18:00:51 +0530

    Grids

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import VsjButton from './components/VsjButton.';
-
+import VsjGrid from './components/VsjGrid';
+import VsjFullWidthGrid from './components/VsjFullWidthGrid';
+import VsjSpacingGrid from './components/VsjSpacingGrid.tsx';
 function App() {
   return (
     <div className="App">
-      <h1>Varanasi Software Junction</h1> 
-      <VsjButton/>    
+      <h1>Varanasi Software Junction</h1>
+      <h2>Full Width</h2>
+<VsjFullWidthGrid/>
+<h2>Grid</h2>
+      <VsjGrid />
+
+      <VsjSpacingGrid/>
     </div>
   );
 }
--- src/components/VsjButton..js
+++ src/components/VsjButton..js
@@ -4,6 +4,9 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import Avatar from '@mui/material/Avatar';
 export default function VsjButton() {
 
+
+  
+
     return (
         <div>
             <center><Avatar alt="Varanasi Software Junction" src="https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png" />
