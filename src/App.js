e0cb5170057058ab333ac2fbe40af7d8fce2e582
Champak's World <champaksworld@gmail.com>
2023-01-23 17:46:26 +0530

    Blog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import VsjDashboard from './components/VsjDashboard';
-
+import Blog from './components/Blog';
 function App() {
   return (
     <div className="App">
-    <VsjDashboard/>
-      
+    <h1>Varanasi Software Junction</h1>
+      <Blog/>
     </div>
   );
 }
