6337b49dde62b5238c27e8559e39f86cb5b46596
champaksworldcreate <champaksworld@gmail.com>
2023-03-26 12:17:48 +0530

    A adder

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 
 // Define the initial state of the Redux store
-const initialState = { count: 0 };
+const initialState = { num1: 0, num2: 0, sum: 0 };
 
 // Define the reducer function that will update the state of the Redux store
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, count: state.count + 1 };
-    case 'DECREMENT':
-      return { ...state, count: state.count - 1 };
+    case 'SET_NUM1':
+      return { ...state, num1: parseInt(action.payload), sum: state.num2 + parseInt(action.payload) };
+    case 'SET_NUM2':
+      return { ...state, num2: parseInt(action.payload), sum: state.num1 + parseInt(action.payload) };
     default:
       return state;
   }
@@ -21,36 +21,42 @@ function reducer(state = initialState, action) {
 // Create the Redux store using the reducer function
 const store = createStore(reducer);
 
-// Define the Counter component
-function Counter(props) {
-  // Define the event handlers for the "increment" and "decrement" buttons
-  const handleIncrement = () => props.dispatch({ type: 'INCREMENT' });
-  const handleDecrement = () => props.dispatch({ type: 'DECREMENT' });
+// Define the Calculator component
+function Calculator(props) {
+  // Define the event handlers for the "num1" and "num2" input fields
+  const handleNum1Change = event => props.dispatch({ type: 'SET_NUM1', payload: event.target.value });
+  const handleNum2Change = event => props.dispatch({ type: 'SET_NUM2', payload: event.target.value });
 
-  // Render the current count value and the "increment" and "decrement" buttons
+  // Render the input fields for "num1" and "num2", and the sum of the two numbers
   return (
     <div>
-      <h1>Count: {props.count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+      <label htmlFor="num1">Number 1:</label>
+      <input type="number" id="num1" value={props.num1} onChange={handleNum1Change} />
+      <br />
+      <label htmlFor="num2">Number 2:</label>
+      <input type="number" id="num2" value={props.num2} onChange={handleNum2Change} />
+      <br />
+      <h1>Sum: {props.sum}</h1>
     </div>
   );
 }
 
-// Define the mapStateToProps function, which maps the state of the Redux store to props for the Counter component
+// Define the mapStateToProps function, which maps the state of the Redux store to props for the Calculator component
 const mapStateToProps = state => {
   return {
-    count: state.count
+    num1: state.num1,
+    num2: state.num2,
+    sum: state.sum
   };
 }
 
-// Connect the Counter component to the Redux store using the connect function
-const ConnectedCounter = connect(mapStateToProps)(Counter);
+// Connect the Calculator component to the Redux store using the connect function
+const ConnectedCalculator = connect(mapStateToProps)(Calculator);
 
-// Render the Counter component inside a Provider component, which provides the Redux store to all child components
+// Render the Calculator component inside a Provider component, which provides the Redux store to all child components
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedCounter />
+    <ConnectedCalculator />
   </Provider>,
   document.getElementById('root')
 );
