2ceb66e6c0401385ced6a688c6b9b837e375887b
champaksworldcreate <champaksworld@gmail.com>
2023-03-26 12:08:27 +0530

    Hello world react.
    Install using
    npm install redux react-redux

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Varanasi Software Junction</h1>
-      <HelloEffect/>      
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'; // Import the React library
+import ReactDOM from 'react-dom'; // Import the ReactDOM library
+import { createStore } from 'redux'; // Import the createStore function from the Redux library
+import { Provider } from 'react-redux'; // Import the Provider component from the react-redux library
 
+// Define the initial state of the Redux store
+const initialState = { message: 'Hello, World!' };
+
+// Define the reducer function that will update the state of the Redux store
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    // Handle any actions here (none in this case)
+    default:
+      return state;
+  }
+}
+
+// Create the Redux store using the reducer function
+const store = createStore(reducer);
+
+// Define the root React component
+function App() {
+  // Render the current state of the Redux store (the "message" property)
+  return (
+    <div>
+      <h1>{store.getState().message}</h1>
+    </div>
+  );
+}
+
+// Render the root React component inside a Provider component, which provides the Redux store to all child components
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
