78388876829508f01b042b7bb5b061ce4aa0eb8d
champaksworldcreate <champaksworld@gmail.com>
2023-12-07 14:42:32 +0530

    Passing the State Up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { useState, useEffect } from "react"; // Importing React hooks and useEffect from react package
-const App = () => {
-  // Functional component named App
-  const f = window.f;
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StateUp from './components/StateUp';
+
+
+//
+
+function App(props) {
+  const[mydata,setMyData]=useState('');
+  const saveData=(n1)=>{
+    
+    const data={...n1};
+    console.log("App");
+    
+    setMyData(n1);
+    console.log(mydata);
+  };
   return (
-    <div className="app-container">
-      <input type="button" onClick={f} value={"Hi"}></input>
-      <h1 onClick={f}>Varanasi Software Junction</h1>
+    <div className="App">
+      <h2>from StateUp in App js {mydata}</h2>
+      <StateUp onSaveData={saveData}/>
+     
+     
     </div>
   );
-};
+}
 
 export default App;
